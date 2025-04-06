b83dac21b7aecb56dd2bc5477e7cab2fc2b97f0e
김정원 <103726899+SF0629@users.noreply.github.com>
2025-04-06 17:28:30 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -35,3 +35,35 @@ for i in range(1,len(data)+p_num+1):
         print(data[i-p_num-1],end='  ')
         
 reset_style()
+
+#------------------------------------------------------------------------
+hamming = int(input(), 2) #전송 하려는 이진수 값 입력 받음
+hamming = list(str(format(hamming, 'b'))) #입력받은 이진수 값 리스트로 전환
+i = 0
+while(1):
+    if 2**i >= len(hamming) + i + 1:
+        parity = i #parity는 페리티 비트의 개수
+        break
+    i += 1
+bit_leng = len(hamming) + parity #총 비트 길이
+
+for k in range(bit_leng): #리스트에 페리티 비트가 들어갈 공간 만들기
+    if k <= parity:
+            hamming.insert(2**k-1, '0')
+    else:
+        continue
+
+bit_leng = len(hamming)
+
+for k in range(parity):
+    index = 2**k
+    xor = 0
+
+    for m in range(1, bit_leng + 1):
+        if m & index and m != index:
+             xor = xor ^ int(hamming[m-1])
+
+    hamming[index - 1] = str(xor)
+
+for k in range(bit_leng-1):
+    print(int(hamming[k]), end = '')
